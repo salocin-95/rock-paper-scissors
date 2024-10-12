698b258a189979379c8241ef040f008f1505e540
Rodrigo <renicolas7@gmail.com>
2024-10-12 13:32:38 -0300

    Modify game.js

--- game.js
+++ game.js
@@ -1,15 +1,32 @@
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const resultsMessage = document.querySelector('.message');
 let playerScore = 0;
 let computerScore = 0;
-
+ 
 function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"]
+    const options = ['rock', 'paper', 'scissors']
     const randomIndex = [Math.floor((Math.random())*options.length)];
     return options[randomIndex];
 }
 
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.querySelector("scissors");
+const userOptions = document.querySelectorAll('.btn');
 
+userOptions.forEach(userOption => userOption.addEventListener('click', (e) => {
+    userOption = e.target.id;
+    if (userOption === getComputerChoice()) {
+        resultsMessage.innerText = "You tie";
+    } else if (userOption === 'rock' && getComputerChoice() === 'scissors') {
+        playerScore++;
+        resultsMessage.innerText = `You win! ${userOption} beats scissors`;
+    } else if (userOption === 'paper' && getComputerChoice() === 'rock') {
+        playerScore++;
+        resultsMessage.innerText = `You win! ${userOption} beats rock`;
+    } else if (userOption === 'scissors' && getComputerChoice() === 'paper') {
+        playerScore++;
+        resultsMessage.innerText = `You win! ${userOption} beats paper`;
+    }
+}));
 
-console.log(rock);
+playerScoreDisplay.innerHTML = `Score: ${playerScore}`
+console.log(playerScore);
